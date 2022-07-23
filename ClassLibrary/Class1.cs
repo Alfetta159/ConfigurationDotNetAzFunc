ac4e3d51d85f039741dbf3a810326689ae68da9c
Daniel R. Przybylski <danp43@outlook.com>
2022-07-22 21:20:20 -0700

    Comments

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -10,6 +10,10 @@ public class Class1
     {
         One = configuration["First"];
 
+        // Always make sure that you let your consumer know exactly what they need for configuration!
+        if (String.IsNullOrWhiteSpace(One))
+            throw new ArgumentException($"The value for 'One' is not configured. Did you forget to add a value for 'First' in you settings?");
+
         Console.WriteLine("Class1 was instantiated.");
     }
 
--- ClassLibrary/StartupExtensions.cs
+++ ClassLibrary/StartupExtensions.cs
@@ -3,30 +3,53 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ClassLibrary;
 
+/// <summary>
+/// A collection of easy to use extension methods allowing consumers to use our library w/o out too much start up pain.
+/// </summary>
+/// <remarks>
+/// When using a settings.json file, configure settings like:
+/// <code>
+/// "DefaultSection":  {
+///     "First": "xx",
+///     "Second": "xx",
+///     "Third": "xx"
+/// },
+/// </code>
+/// </remarks>
 public static class Extensions
 {
+    public const string DefaultSectionName = "DefaultSection";
+
+    /// <summary>Add all classes with the IConfiguration object</summary>
+    /// <remarks>Ironically, we dig out the configuration to find the default section in the settings file.
+    /// It might seem a bit clunky, but it's very reuseable and makes for an easy reference in your consumers' start up files.
+    /// </remarks>
     public static IServiceCollection AddClassLibrary(this IServiceCollection services)
     {
         services
             .AddScoped((sp) =>
             {
                 var configuration = sp.GetService<IConfiguration>();
-                return new Class1(configuration!.GetSection("DefaultSection"));
+                return new Class1(configuration!.GetSection(DefaultSectionName));
             })
             .AddScoped((sp) =>
             {
                 var configuration = sp.GetService<IConfiguration>();
-                return new Class2(configuration!.GetSection("DefaultSection"));
+                return new Class2(configuration!.GetSection(DefaultSectionName));
             })
             .AddScoped((sp) =>
             {
                 var configuration = sp.GetService<IConfiguration>();
-                return new Class3(configuration!.GetSection("DefaultSection"));
+                return new Class3(configuration!.GetSection(DefaultSectionName));
             });
 
         return services;
     }
 
+    /// <summary>Add all classes with the IConfiguration object but with a named configuration section.</summary>
+    /// <remarks>Ironically, we dig out the configuration to find the default section in the settings file.
+    /// It might seem a bit clunky, but it's very reuseable and makes for an easy reference in your consumers' start up files.
+    /// </remarks>
     public static IServiceCollection AddClassLibrary(this IServiceCollection services, string configurationSectionName)
     {
         services
@@ -38,17 +61,21 @@ public static class Extensions
             .AddScoped((sp) =>
             {
                 var configuration = sp.GetService<IConfiguration>();
-                return new Class2(configuration!.GetSection("DefaultSection"));
+                return new Class2(configuration!.GetSection(configurationSectionName));
             })
             .AddScoped((sp) =>
             {
                 var configuration = sp.GetService<IConfiguration>();
-                return new Class3(configuration!.GetSection("DefaultSection"));
+                return new Class3(configuration!.GetSection(configurationSectionName));
             });
 
         return services;
     }
 
+    /// <summary>Add all classes with the IConfiguration object but with configuration that you find in Azure functions.</summary>
+    /// <remarks>We create a configuration as an in-memory collection, which is completely separate from any configuration already in the service collection),
+    /// This is really handy for Azure Functions that rely more on the environment variables collection and not an IConfiguration object.
+    /// </remarks>
     public static IServiceCollection AddClassLibraryFromEnvironment(this IServiceCollection services)
     {
         var configuration = new ConfigurationBuilder()
@@ -68,6 +95,9 @@ public static class Extensions
         return services;
     }
 
+    /// <summary>Add all classes and configure with parameters from the command line.</summary>
+    /// <remarks>It's just like the previous, but were getting command line args from teh Environment.
+    /// </remarks>
     public static IServiceCollection AddClassLibraryFromCommandline(this IServiceCollection services)
     {
         var args = Environment.GetCommandLineArgs();
